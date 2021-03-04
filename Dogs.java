4d146494bff14a74141d96cba49b9fb968be3f41
Kuczok <79452575+Kuczkowski@users.noreply.github.com>
2021-03-04 17:11:00 +0100

    Zadanie 3

---
+++ Dogs.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Dogs extends Animals{
+    public void cats() {
+        System.out.println("To pies:");
+    }
+    public void dogs() {
+        System.out.println("Dog barks");
+    }
+}
---
+++ Zadanie3.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Zadanie3 {
+    public static void main(String[] args) {
+        Cats cat = new Cats();
+        cat.cats();
+        cat.dogs();
+
+        Dogs dog = new Dogs();
+        dog.cats();
+        dog.dogs();
+    } }
